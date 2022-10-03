f1c22f1f85a5947ad540c245a356885f4685ed47
dantesantiago <36947221+dantesantiago@users.noreply.github.com>
2022-10-03 15:34:54 -0700

    Add files via upload

--- PDU_Automation/pdu_py_sel/page_objects/pdu_summary_wp.py
+++ PDU_Automation/pdu_py_sel/page_objects/pdu_summary_wp.py
@@ -1,1 +1,88 @@
+# 2022 - PANDUIT
+#      - Page Object: PDU Summary 
+#      - by dantesan-sada--2022-08-31
+import time
 
+#import sys, os
+#file_dir = os.path.dirname("../utils")
+#sys.path.append(file_dir)
+
+from ..utils import selenium_utils
+
+from ..utils.selenium_utils import wait_and_get_elem_by
+from ..utils.selenium_utils import enter_text
+from ..utils.selenium_utils import click_button
+
+from ..utils.selenium_utils import ID
+from ..utils.selenium_utils import XPATH
+from ..utils.selenium_utils import NAME
+
+# CONSTANTS
+
+# MENU ITEM NAMES
+# username -----------------------------------------
+CHANGE_PASSWORD = 'Change Password'
+USER_ACCOUNTS = "User Accounts"
+LOG_OUT = "Log Out"
+# username -----------------------------------------
+
+# settings -----------------------------------------
+RACK_ACCESS_CONTROL = "Rack Access Control"
+
+# settings -----------------------------------------
+# MENU ITEM NAMES
+
+#svgs aria-label's
+
+AL_HOME = 'home'
+AL_SETTINGS = 'settings-option'
+
+#svgs aria-label's
+
+
+
+# CONSTANTS
+
+# LOCATORS
+
+# LOCATORS
+
+
+#
+# Function name: click_username
+# 
+# Click the username button.
+#
+# dantesan--sada--20022-09-09 
+#
+# driver  - WebDriver
+# username - PDU_WEBPAGE_CLASS_OBJ.username()
+#
+# returns the username button or webelement
+#  
+def click_username(driver, username):
+    username_xpath = "//button/span[text() = '{0}']".format(username)
+    username_bttn = click_button(driver, XPATH, username_xpath)
+    return username_bttn
+
+
+
+#
+# Function name: click_svg_icon
+#
+# Click the icons or svgs given the aria-label.
+#
+# dantesan--sada--20022-09-13 
+#
+# driver  - WebDriver
+# aria_label - the aria label
+#
+# returns the svg webelement
+#  
+def click_svg_icon(driver, aria_label):
+    svg_icon_xpath = "//*[local-name()='svg' and @aria-label = '{0}']".format(aria_label)
+    #svg_icon_we = wait_and_get_elem_by(driver, XPATH, svg_icon_xpath)
+    svg_icon_we = click_button(driver, XPATH, svg_icon_xpath)
+    return svg_icon_we
+
+#---------------------------------- END -------------------------------------
